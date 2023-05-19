4e360069386b6094feffb05bc72403f1b62f0276
JuanC <jc.psp5@gmail.com>
2023-05-19 13:27:58 -0700

    Put settings in separate header, renamed .ino file

---
+++ weather_station_src/station_settings.h
@@ -0,0 +1,14 @@
+#ifndef _STATION_SETTINGS_H_
+#define _STATION_SETTINGS_H_
+
+//WiFi Settings - Update the below to your local WiFi
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+//Location and OpenWeatherMap Settings
+//How to get APIKEY and find CITY_ID: https://www.dmopress.com/openweathermap-howto/
+#define OWM_APIKEY ""
+#define CITY_ID    ""
+#define ALTITUDE   (0.0)
+
+#endif //_STATION_SETTINGS_H_
